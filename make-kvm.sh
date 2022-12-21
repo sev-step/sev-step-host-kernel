e1936c758103e5818dd6a3e3c0fd1524b8d4485a
Luca Wilke <l.wilke@uni-luebeck.de>
2022-12-21 09:40:10 +0000

    refctored kvm module make script
    - narrow usage of root rights
    - improve error display when reloading fails

--- make-kvm.sh
+++ make-kvm.sh
@@ -7,21 +7,18 @@ make -j $cores prepare &&
 make -j $cores modules_prepare &&
 cp /usr/src/linux-headers-`uname -r`/Module.symvers arch/x86/kvm/Module.symvers  &&
 cp /usr/src/linux-headers-`uname -r`/Module.symvers Module.symvers  &&
-chown valentin:valentin arch/x86/kvm/Module.symvers
 cp "/boot/System.map-$(uname -r)" . 
 cp "/boot/System.map-$(uname -r)" arch/x86/kvm/
 touch .scmversion &&
 make -j $cores modules M=arch/x86/kvm/ LOCALVERSION= &&
-make modules_install M=arch/x86/kvm/ LOCALVERSION= &&
+echo "You might be asked for sudo rights, to install the kernel module"
+sudo make modules_install M=arch/x86/kvm/ LOCALVERSION= &&
 
 echo "Unload old modules" 
-modprobe -r kvm_amd kvm 
-cp ./arch/x86/kvm/kvm.ko "/lib/modules/$(uname -r)/kernel/arch/x86/kvm/"
-cp ./arch/x86/kvm/kvm-amd.ko "/lib/modules/$(uname -r)/kernel/arch/x86/kvm/"
+sudo modprobe -r kvm_amd || exit 1
+sudo modprobe -r kvm  || exit 1
+sudo cp ./arch/x86/kvm/kvm.ko "/lib/modules/$(uname -r)/kernel/arch/x86/kvm/"
+sudo cp ./arch/x86/kvm/kvm-amd.ko "/lib/modules/$(uname -r)/kernel/arch/x86/kvm/"
 echo "Load new modules"
-modprobe kvm dbg=1
-modprobe kvm-amd sev=1
-#insmod "/lib/modules/$(uname -r)/kernel/virt/lib/irqbypass.ko"
-#insmod ./arch/x86/kvm/kvm.ko 
-#insmod "/lib/modules/$(uname -r)/kernel/drivers/crypto/ccp/ccp.ko"
-#insmod ./arch/x86/kvm/kvm-amd.ko sev=1 
+sudo modprobe kvm  || exit 1
+sudo modprobe kvm-amd || exit
